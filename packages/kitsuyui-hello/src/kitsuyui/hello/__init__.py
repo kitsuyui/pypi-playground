837c2990519fe5d80fe03abf3428cc02bd960a29
kitsuyui <kitsuyui@kitsuyui.com>
2025-01-04 15:08:43 +0900

    Clean up versioning and exports
    - Export `__version__` in the `__init__.py` of each package.
    - Remove unused `pyproject.toml` entries related to `setuptools_scm` versioning.
    - Enhance `poetry poe clean-all` task to remove `src/*.egg-info` directories.
    - Add docstrings to `__init__.py` files.

--- packages/kitsuyui-hello/src/kitsuyui/hello/__init__.py
+++ packages/kitsuyui-hello/src/kitsuyui/hello/__init__.py
@@ -1,6 +1,33 @@
+"""Hello, World! package.
+
+This package provides a simple function to generate a greeting message "Hello, World!".
+And also provides a function to print the message.
+This package is just for example.
+
+Example:
+    >>> import kitsuyui.hello
+    >>> kitsuyui.hello.hello_world()
+    'Hello, World!'
+    >>> kitsuyui.hello.print_hello_world()
+    Hello, World!
+"""
+
+# https://packaging-guide.openastronomy.org/en/latest/advanced/versioning.html
+from ._version import __version__
+
+
 def hello_world() -> str:
+    """Generate greeting message "Hello, World!"."""
     return "Hello, World!"
 
 
 def print_hello_world():
+    """Print greeting message "Hello, World!"."""
     print(hello_world())
+
+
+__all__ = [
+    "__version__",
+    "hello_world",
+    "print_hello_world",
+]
